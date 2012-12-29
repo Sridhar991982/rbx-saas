5c3fafa9d7cfd7956c7ac816d5389ad89f10e2ce
Alexandre Joseph <jexhson@alexandrejoseph.com>
2012-12-29 19:25:21 +0100

    Ref #27: Transform select into dropdown. Gravatar missing

--- rbx/static/js/rbx.js
+++ rbx/static/js/rbx.js
@@ -1,7 +1,35 @@
 var RBX = (function () {
 
     var _init = function () {
+    	_navTooltips()
+    	_selectToDropdown()
+    }
+
+    var _navTooltips = function () {
         $('.nav a').tooltip({placement: 'bottom'})
+    }
+
+    var _selectToDropdown = function () {
+        $('select').each(function(i, e){
+            if (!($(e).data('convert') == 'no')) {
+                $(e).hide().wrap('<div class="btn-group" id="select-group-' + i + '" />');
+                var select = $('#select-group-' + i);
+                console.log($(e).find(':selected').text())
+                var current = ($(e).val()) ? $(e).find(':selected').text() : '&nbsp;';
+                select.html('<input type="hidden" value="' + $(e).val() + '" name="' + $(e).attr('name') + '" id="' + $(e).attr('id') + '" class="' + $(e).attr('class') + '" />' +
+                            '<a class="btn dropdown-toggle" data-toggle="dropdown" href="#""><span>' + current +
+                            '</span> <span class="caret"></span></a>' +
+                            '<ul class="dropdown-menu"></ul>');
+                $(e).find('option').each(function(o,q) {
+                    select.find('.dropdown-menu').append('<li><a href="#" data-value="' + $(q).attr('value') + '">' + $(q).text() + '</a></li>');
+                    if ($(q).attr('selected')) select.find('.dropdown-menu li:eq(' + o + ')').click();
+                });
+                select.find('.dropdown-menu a').click(function() {
+                    select.find('input[type=hidden]').val($(this).data('value')).change();
+                    select.find('.btn:eq(0) span:eq(0)').text($(this).text());
+                });
+            }
+        });
     }
 
     return {
