290d78b68791ba9c27ba20b189fe80147dd1e873
Alexandre Joseph <jexhson@alexandrejoseph.com>
2013-01-06 22:59:41 +0100

    Ref #46: Improve settings handling to avoid down time

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,9 @@
 from __future__ import with_statement
 
-from os.path import dirname, basename
+from os.path import dirname, basename, join
 from fabric.api import local, run, env, cd
-from fabric.contrib.files import sed
+from fabric.operations import put
+from fabric.contrib.files import sed, exists
 from fabric.contrib.project import rsync_project
 from fabric.context_managers import quiet
 
@@ -10,7 +11,7 @@ env.hosts = ['rbx@ssh.alwaysdata.com']
 PROJECT_NAME = basename(dirname(__file__))
 
 
-def deploy():
+def deploy(configure=False):
     ''' Deploy app on remote server
     '''
     clean()
@@ -20,26 +21,30 @@ def deploy():
         run('ln -fs /usr/local/alwaysdata/python/django/1.4.1/django/' +
             'contrib/admin/static/admin rbx/static')
         run('ln -fs /home/rbx/rbx-django/rbx/static public')
-    settings()
+    is_configured = False
+    if not configure and exists(join(PROJECT_NAME, 'settings.py')):
+        with quiet():
+            is_configured = run('test -z ' +
+                '`grep backends.dummy settings.py`').succeeded
+    else:
+        put('settings.py', PROJECT_NAME)
+    if not is_configured:
+        setup()
     syncdb()
 
 
-def settings():
+def setup():
     ''' Configure remote app settings
     '''
     with cd(PROJECT_NAME):
-        with quiet():
-            is_configured = run('test -n ' +
-                '`grep backends.dummy settings.py`').succeeded
-        if not is_configured:
-            sed('settings.py', 'DEBUG = True', 'DEBUG = False')
-            sed('settings.py', 'default', 'test')
-            sed('settings.py', 'prod', 'default')
+        sed('settings.py', 'DEBUG = True', 'DEBUG = False')
+        sed('settings.py', 'default', 'test')
+        sed('settings.py', 'prod', 'default')
 
-            sed('settings.py', 'django.db.backends.dummy',
-                            'django.db.backends.postgresql_psycopg2')
-            run('sed -i.bak -r -e "s/PASSWORD\': \'\'/PASSWORD\':' +
-                '\'%s\'/g" settings.py' % env.password, quiet=True)
+        sed('settings.py', 'django.db.backends.dummy',
+                        'django.db.backends.postgresql_psycopg2')
+        run('sed -i.bak -r -e "s/PASSWORD\': \'\'/PASSWORD\':' +
+            '\'%s\'/g" settings.py' % env.password, quiet=True)
 
 
 def syncdb():
